800c67580375147a13e51ebf6778a83108382d22
cheng0804 <125541446+cheng0804@users.noreply.github.com>
2024-01-05 10:54:40 +0800

    Update 題目1.py

--- 題目1.py
+++ 題目1.py
@@ -3,3 +3,8 @@ class member:
         self.name = name 
         self.id = id
         self.gender = gender
+c1 = member ("小明","01","男")
+c2 = member ("小華","02","女")
+
+print("名字:",c1.name)
+print("編號:",c2.id)
