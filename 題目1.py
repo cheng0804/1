833695ac88125e0bf5caf67d4fd67ba580a2c2b5
cheng0804 <125541446+cheng0804@users.noreply.github.com>
2024-01-05 11:04:59 +0800

    Update 題目1.py

--- 題目1.py
+++ 題目1.py
@@ -5,6 +5,13 @@ class member:
         self.gender = gender
 c1 = member ("小明","01","男")
 c2 = member ("小華","02","女")
-
+c3 = member ("小王","03","男")
 print("名字:",c1.name)
 print("編號:",c2.id)
+print("名字:",c2.name)
+print("編號:",c1.id)
+print("性別:",c1.gender)
+print("性別:",c2.gender)
+print("名字:",c3.name)
+print("編號:",c3.id)
+print("性別:",c3.gender)
